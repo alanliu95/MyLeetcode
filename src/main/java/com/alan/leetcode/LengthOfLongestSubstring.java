c980951eb83979b2742e03054d728e03e37d3162
alan <438162498@qq.com>
2019-04-24 21:11:31 +0800

    最长不重复子串

---
+++ src/main/java/com/alan/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,54 @@
+package com.alan.leetcode;
+import java.util.*;
+public class LengthOfLongestSubstring {
+    public static void main(String[] args){
+        String s="abcabcbb";
+        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring(s));
+    }
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==0) return 0;
+        Map<Character,Integer> map = new HashMap<>();
+        int head,tail,cnt,max;
+        head=tail=cnt=max=0;
+        while(tail<s.length()){
+            char c=s.charAt(tail);
+            if(!map.containsKey(c)){
+                map.put(c,tail);
+                cnt++;
+            }
+            else{
+                max=Math.max(cnt,max);
+                //int sameIndex=map.get(c);
+                cnt=tail-map.get(c);
+                for(;head<map.get(c);head++)
+                    map.remove(s.charAt(head));
+                map.put(c,tail);
+                head++;
+            }
+            tail++;
+        }
+        max=Math.max(cnt,max);
+        return max;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        int head = 0;
+        int tail = 0;
+        int rlt = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        char[] arr = s.toCharArray();
+        while (tail < arr.length) {
+            if (map.containsKey(arr[tail])) {
+                int index = map.get(arr[tail]);
+                for (int k = head; k <= index; k++) {
+                    map.remove(arr[k]);
+                }
+                head = index + 1;
+            }
+            map.put(arr[tail], tail);
+            rlt = Math.max(rlt, tail - head + 1);
+            tail++;
+        }
+        return rlt;
+    }
+}
