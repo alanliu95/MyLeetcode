11dd2e20265f9b1996c424402279ef32248003db
alan <438162498@qq.com>
2019-10-12 07:55:59 +0800

    1010

--- src/main/java/com/alan/打家劫舍III.java
+++ src/main/java/com/alan/打家劫舍III.java
@@ -3,7 +3,25 @@ package com.alan;
 
 public class 打家劫舍III {
     public int rob(TreeNode root) {
-        return 0;
+        return robCore(root).val;
+    }
+
+    public TreeNode robCore(TreeNode root) {
+        if (root == null) {
+            TreeNode node = new TreeNode(0);
+            return robCore(node);
+        }
+        if (root.left == null && root.right == null) {
+            root.left = new TreeNode(0);
+            root.right = new TreeNode(0);
+            return root;
+        }
+
+        root.left = robCore(root.left);
+        root.right = robCore(root.right);
+        root.val = Math.max(root.val + root.left.left.val + root.left.right.val + root.right.left.val
+                + root.right.right.val, root.left.val + root.right.val);
+        return root;
     }
 }
 
